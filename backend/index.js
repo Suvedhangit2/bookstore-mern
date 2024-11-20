d8425fef84eab31d1bce72f2e67cb2ea457e5c9a
Suvedhangit2 <125052987+Suvedhangit2@users.noreply.github.com>
2024-11-20 15:25:13 +0530

    Update index.js for changing frontend domain

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 // app.use(cors());
 
 app.use(cors({
-  origin: 'https://bookstore-mern-six.vercel.app/',  // hosted frontend domain
+  origin: 'https://bookstore-mern-six.vercel.app/',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
